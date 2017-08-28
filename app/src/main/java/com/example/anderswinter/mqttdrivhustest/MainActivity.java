f911c1e312f3cc6d513b8a881b546cde1d65df2a
Anders Winter <awint15@student.sdu.dk>
2017-08-28 04:33:52 +0200

    Added a broker URL and broker port EditText to the front-end. made a UI for connecting and disconnecting a broker

--- app/src/main/java/com/example/anderswinter/mqttdrivhustest/MainActivity.java
+++ app/src/main/java/com/example/anderswinter/mqttdrivhustest/MainActivity.java
@@ -2,12 +2,84 @@ package com.example.anderswinter.mqttdrivhustest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
 
 public class MainActivity extends AppCompatActivity {
+
+    MqttAndroidClient client;
+    String clientId;
+    Boolean flag = false;
+    EditText EtPort;
+    EditText EtBrokerUI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EtBrokerUI  = (EditText) findViewById(R.id.EtBrokerUI);
+        EtPort      = (EditText) findViewById(R.id.EtPort);
+
+    }
+
+    public  void Connect(View v){
+        clientId = MqttClient.generateClientId();
+        client = new MqttAndroidClient(this.getApplicationContext(), EtBrokerUI.getText().toString() + ":" + EtPort.getText().toString(), clientId);
+        try {
+            IMqttToken token = client.connect();
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+                    Toast.makeText(MainActivity.this,"Connected",Toast.LENGTH_LONG).show();
+                    flag = true;
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    Toast.makeText(MainActivity.this,"Not connected!",Toast.LENGTH_LONG).show();
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Disconnect(View v) {
+        if (flag) {
+            try {
+                flag = false;
+                IMqttToken token = client.disconnect();
+                token.setActionCallback(new IMqttActionListener() {
+                    @Override
+                    public void onSuccess(IMqttToken asyncActionToken) {
+                        // We are connected
+                        Toast.makeText(MainActivity.this, "Disconnected", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                        // Something went wrong e.g. connection timeout or firewall problems
+                        Toast.makeText(MainActivity.this, "Could not disconnect!", Toast.LENGTH_LONG).show();
+
+                    }
+                });
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
+        }else{
+            Toast.makeText(MainActivity.this, "Nothing to disconnect", Toast.LENGTH_LONG).show();
+
+        }
     }
 }
+
